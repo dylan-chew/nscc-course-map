6214b4788e0e86f0ee99c5a77b53094fb58e5834
dylan-chew <dylanw.chew@gmail.com>
2020-04-19 16:18:49 -0300

    working on courses tab, search functionality working

--- courseMap/src/pages/CoursesList.tsx
+++ courseMap/src/pages/CoursesList.tsx
@@ -1,29 +1,97 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   IonContent,
   IonHeader,
   IonPage,
   IonTitle,
   IonToolbar,
+  IonSearchbar,
+  IonRefresher,
+  IonRefresherContent,
+  IonList,
+  IonItem,
 } from "@ionic/react";
-import ExploreContainer from "../components/ExploreContainer";
+import { RefresherEventDetail } from "@ionic/core";
 import "./CoursesList.css";
 
 const CoursesList: React.FC = () => {
+  //define the state of courses
+  const [courses, setCourses] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  const getData = (callback: (data: any) => void) => {
+    const url = "https://w0254970-apim.azure-api.net/courses";
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((json) => {
+        // set the state of our rockets
+        callback(json);
+      });
+  };
+
+  useEffect(() => {
+    console.log("Getting rocket data...");
+
+    getData((data) => {
+      setCourses(data);
+    });
+  }, []);
+
+  const doRefresh = (event: CustomEvent<RefresherEventDetail>) => {
+    console.log("Begin async operation");
+
+    getData((data) => {
+      setCourses(data);
+      event.detail.complete();
+    });
+  };
+
+  const handleInput = (event: any) => {
+    setSearchText(event.detail.value);
+  };
+
+  let coursesFiltered = courses
+    .filter((course: any) => {
+      if (
+        searchText === "" ||
+        course.Title.toLowerCase().includes(searchText.toLowerCase()) ||
+        course.CourseCode.toLowerCase().includes(searchText.toLowerCase())
+      ) {
+        return course;
+      }
+    })
+    .map((course: any, i) => {
+      if (course)
+        return (
+          <IonItem key={i}>
+            {course.CourseCode} - {course.Title}
+          </IonItem>
+        );
+    });
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Courses</IonTitle>
+          <IonTitle>courses</IonTitle>
         </IonToolbar>
       </IonHeader>
+      <IonToolbar>
+        <IonSearchbar onIonChange={handleInput}></IonSearchbar>
+      </IonToolbar>
       <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <IonRefresher slot="fixed" onIonRefresh={doRefresh}>
+          <IonRefresherContent
+            pullingIcon="arrow-dropdown"
+            pullingText="Pull to refresh"
+            refreshingSpinner="circles"
+            refreshingText="Refreshing..."
+          ></IonRefresherContent>
+        </IonRefresher>
+        <IonList>
+          {coursesFiltered.length > 0 ? coursesFiltered : "No Courses Found"}
+        </IonList>
       </IonContent>
     </IonPage>
   );
